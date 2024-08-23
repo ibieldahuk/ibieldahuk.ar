f1a9a0e8b291230ad16b97107f3c2b78154961d0
Ibi Eldahuk <ibieldahuk@gmail.com>
2024-08-22 22:11:30 -0300

    feat: encabezado creado

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,1 +1,27 @@
-¡Hola, mundo!
+<html>
+
+	<head>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Holtwood+One+SC&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="{{ asset('css/encabezado.css') }}">
+	</head>
+
+	<body>
+
+		<header>
+			<div id="logotipo">ibi.<span>e</span></div>
+			<div id="botones-nav">
+				<div id="primer-boton">
+					Inicio
+				</div>
+				<div>
+					Mis Proyectos
+				</div>
+			</div>
+		</header>
+
+	</body>
+
+</html>
