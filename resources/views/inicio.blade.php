0ad54d46237a0f29c60c32b53c86d80b1a8620e3
Ibi Eldahuk <ibieldahuk@gmail.com>
2024-08-27 13:43:19 -0300

    feat: sección de proyectos destacados creada

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,26 +1,79 @@
 <html>
 
 	<head>
+
+		<!-- FUENTES -->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Holtwood+One+SC&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+		<!-- HOJAS DE ESTILO -->
+		<link rel="stylesheet" href="{{ asset('css/comun.css') }}">
 		<link rel="stylesheet" href="{{ asset('css/encabezado.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
+
 	</head>
 
 	<body>
 
 		<header>
 			<div id="logotipo">ibi.<span>e</span></div>
-			<div id="botones-nav">
+			<nav id="botones-nav">
 				<div id="primer-boton">
 					Inicio
 				</div>
 				<div>
 					Mis Proyectos
 				</div>
-			</div>
+			</nav>
 		</header>
+
+		<main>
+
+			<section>
+				<h2 class="c-whiteish">Proyectos destacados</h2>
+			</section>
+
+			<section class="contenedor-grid c-whiteish">
+				<div class="d-grid d-rows-50-50 item-1">
+					<div class="d-flex d-centrado-flex">
+						<svg class="tam-img-miniatura" fill="#E8BCB9" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <rect x="9" y="15" width="6" height="2"></rect> <rect x="9" y="12" width="6" height="2"></rect> <path d="M19,7V6h-1V5h-1V4h-1V3h-1V2H5v1H4v18h1v1h14v-1h1V7H19z M18,19h-1v1H7v-1H6V5h1V4h5v6h6V19z M16,7v1h-2V6h1v1H16z"></path> </g> </g></svg>
+					</div>
+					<div class="d-flex d-columna-flex d-centrado-flex-between">
+						<h3 class="c-bg-oscuro a-centrado">PROYECTO 1</h3>
+						<p class="t-medio-1 a-justificado">Four pounds... foooour pounds as if two pounds wasn't bad enough. we're talking two three hundred boxes of sinus pills there ain't that many Smurfs in the world. So no sudo? So you do have a plan! Yeah, Mr. White! Yeah, science!</p>
+					</div>
+				</div> 
+				<div class="d-grid d-cols-50-50">
+					<div class="d-flex d-centrado-flex">
+						<svg class="tam-img-miniatura" fill="#E8BCB9" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M6,12h1v1h6v-1h1v-1h1V5h-1V4h-1V3H7v1H6v1H5v6h1V12z M7,7h1V6h1V5h2v1h1v1h1v2h-1v1h-1v1H9v-1H8V9H7V7z"></path> <polygon points="16,15 15,15 15,14 4,14 4,15 3,15 3,16 2,16 2,21 4,21 4,18 5,18 5,17 6,17 6,16 13,16 13,17 14,17 14,18 15,18 15,21 17,21 17,16 16,16 "></polygon> <polygon points="18,12 19,12 19,11 20,11 20,5 19,5 19,4 18,4 18,3 17,3 16,3 16,6 17,6 17,7 18,7 18,8 18,9 17,9 17,10 16,10 16,13 18,13 "></polygon> <polygon points="21,16 21,15 20,15 20,14 18,14 18,17 19,17 19,18 20,18 20,21 22,21 22,16 "></polygon> </g> </g></svg>
+					</div>
+					<div class="d-flex d-columna-flex d-centrado-flex-between">
+						<h4 class="c-bg-oscuro a-centrado">PROYECTO 2</h4>
+						<p class="t-medio-2 a-justificado">Yo... it's appointment only! Jeez, you look like... Lex Luthor. I got two dudes turned into raspberry slushy and flushed down my toilet. I can't even take a proper dump in there. I mean the whole damn house has gotta be haunted by now.</p>
+					</div>
+				</div> 
+				<div class="d-grid d-cols-50-50">
+					<div class="d-flex d-centrado-flex">
+						<svg class="tam-img-miniatura" fill="#E8BCB9" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon points="20,4 20,6 18,6 18,8 16,8 16,10 14,10 14,12 12,12 12,14 10,14 10,16 9,16 9,17 7,17 7,16 6,16 6,14 4,14 4,13 2,13 2,16 4,16 4,18 6,18 6,20 7,20 7,21 9,21 9,20 10,20 10,18 12,18 12,16 14,16 14,14 16,14 16,12 18,12 18,10 20,10 20,8 22,8 22,4 "></polygon> </g></svg>
+					</div>
+					<div class="d-flex d-columna-flex d-centrado-flex-between">
+						<h4 class="c-bg-oscuro a-centrado">PROYECTO 3</h4>
+						<p class="t-medio-2 a-justificado">You didn't actually see Tuco? You got this money from Tuco? Tuco gave you this is what you're saying? You made a deal? How... why would you make a deal with that scumbag? You see what he did to me? No way man, okay, no understanding!</p>
+					</div>
+				</div> 
+			</section>
+
+			<section>
+				<a class="enlace t-medio-2 c-whiteish" href="#">
+					<img src="{{ asset('assets/RNSA.gif') }}">
+					Ver más proyectos
+					<img src="{{ asset('assets/RNSA.gif') }}">
+				</a>
+			</section>
+
+		</main>
 
 	</body>
 
